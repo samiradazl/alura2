c9df8a06ea0e25e516263077e933681e50fe3d17
samiradazl <samiramaciel23barbosa@gmail.com>
2024-10-10 07:56:53 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'harry potter',
+    'QUEM É O PERSONAGEM PRINCIPAL?',
+    'HARRY POTTER'
+)
+
+criaCartao(
+    'NARUTO',
+    'QUAL E O NOME VERDADEIRO DO FILHO DO NARUTO',
+    'BORUTO'
+)
+
+criaCartao(
+    'CURIOSIDADE',
+    'EM QUE ANO OCORREU A SEGUNDA GUERRA MUNDIAL?',
+    '1939 ATÉ 1945'
+)
+
+criaCartao(
+    'FATOS',
+    'QUAL É A MATÉRIA MAIS CHATA DA ESCOLA?',
+    'FÍSICA E GEOGRAFIA'
+)
